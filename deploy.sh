653eca8a9c38347da11597c06bd7d5b3f6acd1ae
Tommaso Tedeschi <43155805+ttedeschi@users.noreply.github.com>
2020-07-13 11:20:44 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-kubectl apply -f manifests_no_configs/httpgo_and_exporter.yaml
-kubectl apply -f manifests_no_configs/httpd_and_exporter.yaml
-kubectl apply -f manifests_no_configs/couchdb_and_exporter.yaml
+kubectl apply -f manifests_no_configs/httpgo_and_exporter.yaml --validate=false
+kubectl apply -f manifests_no_configs/httpd_and_exporter.yaml --validate=false
+kubectl apply -f manifests_no_configs/couchdb_and_exporter.yaml --validate=false
 kubectl create configmap prometheus-example-cm --from-file configs/prometheus.yml
+kubectl create namespace monitoring
 kubectl apply -f manifests_no_configs/prometheus.yaml
 kubectl create configmap prometheus-example-cm --from-file configs/prometheus_adapter.yml
 kubectl apply -f manifests_no_configs/prometheus_adapter.yaml
-kubectl apply -f manifests_no_configs/hpa_hpptgo.yaml
+kubectl apply -f manifests_no_configs/hpa_httpgo.yaml
 kubectl apply -f manifests_no_configs/hpa_httpd.yaml
 kubectl apply -f manifests_no_configs/hpa_couchdb.yaml
