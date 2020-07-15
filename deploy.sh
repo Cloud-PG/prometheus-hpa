1c9db5bfab9b765cfe5bfbdcd0c1bf503bf3f030
Tommaso Tedeschi <tedesk95@gmail.com>
2020-07-15 13:03:46 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ kubectl apply -f manifests_no_configs/couchdb_and_exporter.yaml --validate=false
 kubectl create configmap prometheus-example-cm --from-file configs/prometheus.yml
 kubectl create namespace monitoring
 kubectl apply -f manifests_no_configs/prometheus.yaml
-kubectl create configmap prometheus-example-cm --from-file configs/prometheus_adapter.yml
+kubectl create configmap prometheus-adapter-example-cm --from-file configs/prometheus_adapter.yml
 kubectl apply -f manifests_no_configs/prometheus_adapter.yaml
 kubectl apply -f manifests_no_configs/hpa_httpgo.yaml
 kubectl apply -f manifests_no_configs/hpa_httpd.yaml
