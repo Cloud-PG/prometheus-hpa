c4960971d9003fbb9d0510e7fa3cb6556bebd4f7
Tommaso Tedeschi <tedesk95@gmail.com>
2020-08-18 15:26:24 +0200

    put different apps in different namespaces

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-kubectl apply -f manifests_no_configs/httpgo_and_exporter.yaml --validate=false
-kubectl apply -f manifests_no_configs/httpd_and_exporter.yaml --validate=false
-kubectl apply -f manifests_no_configs/couchdb_and_exporter.yaml --validate=false
+kubectl create namespace http
+kubectl create namespace frontend
+kubectl create namespace database
+kubectl apply -f manifests_no_configs/httpgo_and_exporter.yaml --validate=false -n http
+kubectl apply -f manifests_no_configs/httpd_and_exporter.yaml --validate=false -n frontend
+kubectl apply -f manifests_no_configs/couchdb_and_exporter.yaml --validate=false -n database
 kubectl create namespace monitoring
 kubectl create configmap prometheus-example-cm --from-file configs/prometheus.yml -n monitoring
 kubectl apply -f manifests_no_configs/prometheus.yaml
